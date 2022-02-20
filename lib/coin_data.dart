57d7954efc79e209bd069a7adf8fc66ca1fd3d0a
Mina Emil <minaemil329@gmail.com>
2022-02-20 07:33:52 +0200

    Add API

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,9 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+const apikey = '708EDBF1-4DF1-4F4D-A5B8-05DDA12D51D1' ;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +34,16 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData  {
+  Future<double> getCoinData(String value) async {
+    http.Response response = await http.get(Uri.parse('https://rest.coinapi.io/v1/exchangerate/BTC/$value?apikey=$apikey'));
+    if(response.statusCode == 200){
+      var JsonBody =response.body;
+      var DecodedData =await jsonDecode(JsonBody);
+      return DecodedData['rate'];
+    }else{
+      print(response.statusCode);
+      return 0 ;
+    }
+  }
+}
